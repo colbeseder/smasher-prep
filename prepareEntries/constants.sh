7c3ecbf4a9f2dd04c139296f3c109fd2e334e292
Bryan Appleby <bryanjappleby@gmail.com>
2022-02-23 21:50:03 +0200

    Centralize constants files

--- prepareEntries/constants.sh
+++
@@ -1,3 +0,0 @@
-export mongoURI="mongodb://mongodb-service.mongodb-namespace.svc.cluster.local:27017"
-export API_Gateway="http://apigate.backend-namespace.svc.cluster.local:3000"
-echo "Constants sourced"
