ee16e6c8d6057fec8ec9a9d1bcfe64fef9410abe
Bryan <bryanjappleby@gmail.com>
2022-02-25 12:30:11 +0200

    Prepare raw queue in prepEntries container (#15)

--- prepareEntries/app.js
+++ prepareEntries/app.js
@@ -1,35 +1,40 @@
-const axios = require('axios')
+const axios = require('axios');
+const { clear } = require('console');
 const prepareEntry = require('./prep');
 
 var apiURI = process.argv[2]
-var interval = 20;
-
-function prepareNextEntry(){
-    axios.get(apiURI + "/api/q")
-        .then(function(res){
-            if (!/[a-z]/i.test(res?.data?._id)){
-                // Queue is empty?
-                interval *= 2;
-                interval = Math.min(interval, 5*60*1000);
-                return;
-            }
-            else {
-                interval = 20;
-            }
-            //console.log(res)
-            prepareEntry(res.data._id, insertEntry)
-        })
-        .catch(er => {
-            interval *= 2;
-        });
-    setTimeout(prepareNextEntry, interval);
-}
 
 function insertEntry(entry){
-    console.log(entry)
     if (entry.start && entry.end && entry.clue){
+        //console.log(`Writing: ${entry.title}`)
         axios.post(apiURI + "/api/entry/" + encodeURIComponent(entry.title), entry).catch();
     }
 }
 
-prepareNextEntry()
+var queue = [];
+
+function insertAllRaw(){
+    var lineReader = require('readline').createInterface({
+      input: require('fs').createReadStream('resources/titles')
+    });
+    
+    lineReader.on('line', function (line) {
+        var word = line.trim().toLocaleLowerCase();
+        if (/^[a-z'\-]+$/i.test(word)){
+            queue.push(word);
+         }
+     });
+}
+
+var handle;
+function dequeue(){
+    if (queue.length === 0){
+        clearInterval(handle);
+        return;
+    }
+    prepareEntry(queue.pop(), insertEntry);
+}
+
+insertAllRaw();
+console.log(queue.slice(0,5).join());
+handle = setInterval(dequeue, 20);
--- prepareEntries/start.sh
+++ prepareEntries/start.sh
@@ -1,2 +1,6 @@
 source constants.sh
+# Load the raw words into the database for prepare the entries
+grep "^[a-z]\{3,\}" "resources/common-words.txt" > resources/ordered_titles
+cat resources/ordered_titles | shuf > resources/titles
+
 node app.js $API_Gateway
