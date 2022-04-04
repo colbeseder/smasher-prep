e0337920d296d3c8895452d5dd72fbd8b15c9169
Bryan Appleby <bryanjappleby@gmail.com>
2022-04-04 20:36:42 +0300

    Local deploy uses test_words.txt list

--- prepareEntries/start.sh
+++ prepareEntries/start.sh
@@ -1,5 +1,5 @@
 #source constants.sh
 # Load the raw words into the database for prepare the entries
-node ./removePlurals.js "resources/default-words.txt" | shuf > resources/titles
+node ./removePlurals.js $WORD_LIST | shuf > resources/titles
 
 node app.js $API_Gateway
